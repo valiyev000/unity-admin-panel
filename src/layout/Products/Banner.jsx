d4651faf53e6f76b2376dfee71ec6d7a0059728f
valiyev000 <veliev1203@gmail.com>
2024-01-24 14:30:28 +0400

    some updates on the product page

--- src/layout/Products/Banner.jsx
+++ src/layout/Products/Banner.jsx
@@ -9,11 +9,48 @@ function Banner() {
     const { screenWidth, translation, theme, setIsSettingOpen } = useContext(contextApi)
 
     return (
-        <motion.div layout className={styles.main} style={{ margin: screenWidth > 480 ? "40px 0 40px 0" : "0 0 40px 0", height: screenWidth < 480 ? "540px" : "256px" }}>
-            <motion.div layout className={styles.left} style={{marginTop: screenWidth < 480 ? "256px" : "0px" , alignItems: screenWidth < 480 ? "center" : "flex-start"}}>
+        <motion.div
+            className={styles.main}
+            layout
+            style={{
+                margin: screenWidth > 480 ? "40px 0 40px 0" : "0 0 40px 0",
+                padding: screenWidth > 480 ? "40px 54px" : "40px 0",
+                height: screenWidth < 480 ? "unset" : "256px",
+                justifyContent: screenWidth > 480 ? "space-between" : "center"
+            }}
+        >
+            <motion.div layout className={styles.left} style={{ marginTop: screenWidth < 480 ? "256px" : "0px", alignItems: screenWidth < 480 ? "center" : "flex-start" }}>
                 <div className={styles.title}>{translation.your_products}</div>
                 <div className={styles.content}>{translation.Create_your_product_dashboard_in_minutes}</div>
-                <motion.button layout className={styles.btn} initial={{transform: "scale(1)"}} animate={{transform: "scale(1)"}} whileHover={{transform: "scale(0.95)"}} onClick={() => setIsSettingOpen(true)} style={{ background: theme === "dark" ? '#242731' : "#fff", color: theme === "dark" ? '#fff' : "#11142D" }}>{translation.check_all_setting}</motion.button>
+                <motion.button
+                    className={styles.btn}
+                    layout
+                    initial={{ transform: "scale(1)" }}
+                    animate={{ transform: "scale(1)" }}
+                    whileHover={{ transform: "scale(0.95)" }}
+                    onClick={() => setIsSettingOpen(true)}
+                    style={{
+                        background: theme === "dark" ? '#242731' : "#fff",
+                        color: theme === "dark" ? '#fff' : "#11142D",
+                        marginBottom: "15px"
+                    }}
+                >
+                    {translation.check_all_setting}
+                </motion.button>
+                {/* <motion.button
+                    className={styles.btn}
+                    layout
+                    initial={{ transform: "scale(1)" }}
+                    animate={{ transform: "scale(1)" }}
+                    whileHover={{ transform: "scale(0.95)" }}
+                    onClick={() => setIsSettingOpen(true)}
+                    style={{
+                        background: theme === "dark" ? '#242731' : "#fff",
+                        color: theme === "dark" ? '#fff' : "#11142D"
+                    }}
+                >
+                    {translation.add_new_product}
+                </motion.button> */}
             </motion.div>
             <img
                 className={styles.img}
